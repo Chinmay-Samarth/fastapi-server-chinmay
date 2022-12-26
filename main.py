410288c682a8469975670cbcc330e462e4037705
Chinmay <chinmaydhondale@gmail.com>
2022-12-26 13:28:51 +0530

    14

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run("dist.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("dist.api:app", host="0.0.0.0", port=0000, reload=True)
