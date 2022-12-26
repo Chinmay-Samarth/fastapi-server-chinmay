0674e68ccffd92aeaf6fa0b4e41fe4a39135d766
Chinmay <chinmaydhondale@gmail.com>
2022-12-26 13:58:42 +0530

    22

--- dist/database.py
+++ dist/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./user.db?mode=rw'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False},)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,echo=True, connect_args={'check_same_thread': False})
+engine.connect()
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
