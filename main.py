1b0179e01c9aa358214f97bf6fa55a7f66fcc49d
Chinmay <chinmaydhondale@gmail.com>
2022-12-27 13:09:40 +0530

    1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#
+
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run("api:app", host="0.0.0.0", port=0000, reload=True)
+    uvicorn.run("hello:app", host="0.0.0.0", port=0000, reload=True)
